0f6c2565e21497f0fb27720b395c05e20c5c7ba7
sushil <sushil.sree98@gmail.com>
2020-02-18 13:17:45 +0530

    Add files via upload

---
+++ node/index.js
@@ -0,0 +1,24 @@
+let express=require('express');
+let app=express();
+let bodyParser=require('body-parser');
+let cors=require('cors');
+let array=[];
+app.use(bodyParser.json());
+
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+app.use(cors({
+  origin : "",
+}
+));
+app.get("/value",function(req,res){
+  res.json(array);
+})
+app.listen(8000);
+
+app.post("/value",function(req,res){
+  array.push(req.body);
+  console.log(req.body);
+  res.json(req.body);
+})
